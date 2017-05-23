b53a3536af26f2bbf72d73ee1e9c2d11387c8407
Sebastian Muszynski <muszynski.sebastian@gmail.com>
2017-05-23 22:44:36 +0200

    Sky city preview

--- client/View.elm
+++ client/View.elm
@@ -1,7 +1,7 @@
 module View exposing (..)
 
 import AFrame exposing (scene, entity)
-import AFrame.Primitives exposing (assets, assetItem, plane, box, sphere, text, objModel)
+import AFrame.Primitives exposing (assets, assetItem, plane, box, sphere, text, objModel, image, sky)
 import AFrame.Primitives.Attributes exposing (..)
 import AFrame.Primitives.Camera exposing (camera)
 import AFrame.Primitives.Cursor exposing (cursor, timeout, fuse)
@@ -30,7 +30,8 @@ view model =
                             , attribute "data-player-id" player.id
                             ]
                             [ assets []
-                                [ assetItem
+                                [ image [ id "sky", src "images/sky2.jpg" ] []
+                                , assetItem
                                     [ id "mario-mtl"
                                     , src "models/mario/mario-sculpture.mtl"
                                     ]
@@ -52,9 +53,11 @@ view model =
                                     []
                                 ]
                             , renderCamera (Position 0 0.6 0) player.points
-                            , renderFloor
-                            , renderPlayers model.game
-                            , renderEnemies model.game.enemies
+
+                            -- , renderFloor
+                            -- , renderPlayers model.game
+                            -- , renderEnemies model.game.enemies
+                            , renderSky
                             ]
 
                     Nothing ->
@@ -91,8 +94,10 @@ renderCamera cameraPos points =
             , attribute "player-position-listener" "true"
             ]
             [ renderCursor
-            , renderPoints points
+
+            -- , renderPoints points
             ]
+        , renderPlayerLight cameraPos
         ]
 
 
@@ -117,6 +122,15 @@ renderFloor =
             ]
             []
         ]
+
+
+renderSky : Html msg
+renderSky =
+    sky
+        [ src "#sky"
+        , attribute "animation" ("property: rotation; dir: alternate; dur: 2000; easing: easeInOutSine; loop: true; to: 0.5 0.5 0.1")
+        ]
+        []
 
 
 renderPlayers : Game -> Html msg
@@ -144,7 +158,7 @@ renderPlayer player =
 renderEnemies : List Enemy -> Html msg
 renderEnemies enemies =
     entity
-        [ attribute "animation" ("property: rotation; dir: alternate; dur: 10000; easing: easeInOutSine; loop: true; to: 5 5 5")
+        [ attribute "animation" ("property: rotation; dur: 10000; easing: easeInOutSine; loop: true; to: 0 360 0")
         ]
         (List.map renderEnemy enemies)
 
@@ -153,21 +167,11 @@ renderEnemy : Enemy -> Html msg
 renderEnemy enemy =
     let
         newPosition =
-            [ enemy.position.x, enemy.position.y + 2, enemy.position.z ]
+            [ enemy.position.x, enemy.position.y + 4, enemy.position.z ]
                 |> List.map toString
                 |> List.intersperse " "
                 |> String.concat
     in
-        -- sphere
-        --     [ attribute "data-id" enemy.id
-        --     , position enemy.position.x enemy.position.y enemy.position.z
-        --     , radius 0.5
-        --     , color (rgb 255 0 0)
-        --     , attribute "enemy-hover-listener" "true"
-        --     , attribute "visible" (String.toLower <| toString enemy.isVisible)
-        --     , attribute "animation" ("property: position; dir: alternate; dur: 2000; easing: easeInOutSine; loop: true; to: " ++ newPosition)
-        --     ]
-        --     []
         objModel
             [ src "#draug-obj"
             , attribute "mtl" "#draug-mtl"
@@ -187,5 +191,15 @@ renderPoints points =
         [ attribute "value" (toString points)
         , color (rgb 0 0 0)
         , position 1 0 -2
+        ]
+        []
+
+
+renderPlayerLight : Position -> Html msg
+renderPlayerLight playerPosition =
+    entity
+        [ attribute "light" "angle: 90; color: #fff; decay: 0; intensity: 1; penumbra: 1; type: spot"
+        , position playerPosition.x 4 playerPosition.z
+        , rotation -90 0 0
         ]
         []
