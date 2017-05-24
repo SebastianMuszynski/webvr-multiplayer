74bbc3ced986295c06a33a228da926e836899b7b
Sebastian Muszynski <muszynski.sebastian@gmail.com>
2017-05-24 19:39:24 +0200

    Update player position only when changed

--- client/aframe-components/playerPositionListener.js
+++ client/aframe-components/playerPositionListener.js
@@ -1,34 +1,38 @@
 AFRAME.registerComponent('player-position-listener', {
-  tick: function (time) {
+  init: function () {
     var player = this.el;
     var scene = player.sceneEl;
     var playerId = scene.getAttribute("data-player-id");
 
-    if (time - this.time < 50) {
-      return;
-    }
+    player.addEventListener('componentchanged', function (evt) {
+      var propChanged = evt.detail.name;
+      var hasPositionChanged = propChanged == 'position' || propChanged == 'rotation';
 
-    this.time = time;
-
-    var playerPosition = player.getAttribute('position');
-    var playerRotation = player.getAttribute('rotation');
-
-    playerRotation.x = Math.floor(playerRotation.x);
-    playerRotation.y = Math.floor(playerRotation.y);
-    playerRotation.z = Math.floor(playerRotation.z);
-
-    var playerSettings = {
-      position: playerPosition,
-      rotation: playerRotation,
-    };
-
-    var action = {
-      type_: "PLAYER_POSITION_CHANGED",
-      payload: {
-        data: JSON.stringify(playerSettings),
-        player_id: playerId || ""
+      if (!hasPositionChanged) {
+        return;
       }
-    };
-    window.elmApp.ports.fromJs.send(JSON.stringify(action));
+
+      var playerPosition = player.getAttribute('position');
+      var playerRotation = player.getAttribute('rotation');
+
+      playerRotation.x = Math.floor(playerRotation.x);
+      playerRotation.y = Math.floor(playerRotation.y);
+      playerRotation.z = Math.floor(playerRotation.z);
+
+      var playerSettings = {
+        position: playerPosition,
+        rotation: playerRotation,
+      };
+
+      var action = {
+        type_: "PLAYER_POSITION_CHANGED",
+        payload: {
+          data: JSON.stringify(playerSettings),
+          player_id: playerId || ""
+        }
+      };
+
+      window.elmApp.ports.fromJs.send(JSON.stringify(action));
+    });
   }
 });
