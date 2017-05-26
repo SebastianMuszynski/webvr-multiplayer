6ad9d0ed7f8adee2d7d019860854f52c36575781
Sebastian Muszynski <muszynski.sebastian@gmail.com>
2017-05-26 21:39:04 +0200

    Remove player position listener

--- client/View.elm
+++ client/View.elm
@@ -82,7 +82,8 @@ renderCamera cameraPos points =
         []
         [ camera
             [ position cameraPos.x 5 cameraPos.z
-            , attribute "player-position-listener" "true"
+
+            -- , attribute "player-position-listener" "true"
             ]
             [ renderCursor
 
