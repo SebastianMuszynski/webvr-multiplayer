168b1d24aab24bb68d4ad61b8c186f2772f416bf
Sebastian Muszynski <muszynski.sebastian@gmail.com>
2017-09-22 14:48:00 +0200

    Fix enemy color

--- src/services/AddPlayerService.cr
+++ src/services/AddPlayerService.cr
@@ -1,7 +1,7 @@
 class AddPlayerService
   def self.call(player : Player, game : Game)
-    game.add_player(player)
     player.set_color(game.scene.get_new_player_color) # TODO: UGLY!
+    game.add_player(player)
 
     SocketsHelper.unicast(player, Action.new_player(player.id))
     SocketsHelper.broadcast(game.players, Action.players(game.players))
